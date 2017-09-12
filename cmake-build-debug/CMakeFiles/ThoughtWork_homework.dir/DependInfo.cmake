6536025551680a46da9db09e40b24ab2cf6019a7
EStormLynn <zhang3158627@sina.com>
2017-09-12 14:48:43 +0800

    CMAKE

---
+++ cmake-build-debug/CMakeFiles/ThoughtWork_homework.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/SeeKHit/ThoughtWork_homework/TWIO.cpp" "/Users/SeeKHit/ThoughtWork_homework/cmake-build-debug/CMakeFiles/ThoughtWork_homework.dir/TWIO.cpp.o"
+  "/Users/SeeKHit/ThoughtWork_homework/TWOrderManager.cpp" "/Users/SeeKHit/ThoughtWork_homework/cmake-build-debug/CMakeFiles/ThoughtWork_homework.dir/TWOrderManager.cpp.o"
+  "/Users/SeeKHit/ThoughtWork_homework/main.cpp" "/Users/SeeKHit/ThoughtWork_homework/cmake-build-debug/CMakeFiles/ThoughtWork_homework.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ThoughtWork_homework.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThoughtWork_homework.dir/main.cpp.o"
+  "CMakeFiles/ThoughtWork_homework.dir/TWOrderManager.cpp.o"
+  "CMakeFiles/ThoughtWork_homework.dir/TWIO.cpp.o"
+  "ThoughtWork_homework.pdb"
+  "ThoughtWork_homework"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ThoughtWork_homework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
